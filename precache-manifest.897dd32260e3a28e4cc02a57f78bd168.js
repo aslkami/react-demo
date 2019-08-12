be7d714edcbb6f493badcb81712686555d174dac
aslkami <aslkami@yahoo.com>
2019-08-12 14:27:29 +0800

    Updates

---
+++ precache-manifest.897dd32260e3a28e4cc02a57f78bd168.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bcafa162553a31aca12d765b127342",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "bed5cbc7f4061c3ee971",
+    "url": "/react-demo/static/css/2.6bb96386.chunk.css"
+  },
+  {
+    "revision": "9948ff0529d0746147fe",
+    "url": "/react-demo/static/css/main.85024160.chunk.css"
+  },
+  {
+    "revision": "bed5cbc7f4061c3ee971",
+    "url": "/react-demo/static/js/2.8d618c7c.chunk.js"
+  },
+  {
+    "revision": "9948ff0529d0746147fe",
+    "url": "/react-demo/static/js/main.ff32d7c8.chunk.js"
+  },
+  {
+    "revision": "78404b4b0f010a1ef451",
+    "url": "/react-demo/static/js/runtime~main.3dcdfef1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-demo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-demo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-demo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-demo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-demo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
